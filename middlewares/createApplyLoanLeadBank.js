77d3ecbe611b819dde588daf3bee7a7c1f714b8c
Jaydeep Sarkar <jaydeep.sarkar@intglobal.com>
2026-01-02 12:07:02 +0530

    fix: remove no use calls

--- middlewares/createApplyLoanLeadBank.js
+++ middlewares/createApplyLoanLeadBank.js
@@ -1,58 +1,7 @@
-const request = require("request");
 const thirdPartyApi = require("../utils/thirdPartyApi");
-const apiFetcher = require("../utils/apiFetcher");
 const { logger } = require("../utils/logger");
 const { sendRequest } = require("../utils/thirdPartyApiService");
 const { saveApplyLoanData } = require("../models/userModel");
-
-/*const createApplyLoanLeadBank = async (req, res, next) => {
-  const requestData = req.body;
-  try {
-    const fetchLoanLeadBank = (callback) => {
-      const options = {
-        method: thirdPartyApi.getApplyLoanLeadBank.method,
-        url: thirdPartyApi.getApplyLoanLeadBank.endpoint,
-        headers: thirdPartyApi.getApplyLoanLeadBank.headers,
-        data: JSON.stringify(requestData),
-    //data: requestData,
-    proxy: "http://10.130.1.1:8080" 
-      };
-    console.log(typeof requestData)
-  console.log({
-        method: thirdPartyApi.getApplyLoanLeadBank.method,
-        url: thirdPartyApi.getApplyLoanLeadBank.endpoint,
-        headers: thirdPartyApi.getApplyLoanLeadBank.headers,
-        //data: JSON.stringify(requestData),
-    data: requestData,
-    proxy: "http://10.130.1.1:8080" 
-      })
-      request(options, function (error, response) {
-        if (error) {
-          callback(error, null);
-        } else {
-          const apiResponse = response.body;
-          callback(null, apiResponse);
-        }
-      });
-    };
-
-    fetchLoanLeadBank((error, apiResponse) => {
-      if (error) {
-        console.error(error);
-      } else {
-        req.apiData = apiResponse;
-        next();
-      }
-    });
-  } catch (error) {
-    //console.log("Error is fetching data from server:", error);
-    res.status(500).json({
-      success: false,
-      statuse: 500,
-      message: "Internal Server Error",
-    });
-  }
-};*/
 
 const createApplyLoanLeadBank = async (req, res, next) => {
   try {
